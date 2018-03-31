1c356608a6640301e906d4c63e2f955255ef27cf
jogi323 <jogigj1408@gmail.com>
2018-03-31 08:40:26 +0530

    responsive and state is defined

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -4,7 +4,28 @@ import * as FontAwesome from 'react-icons/lib/fa';
 import '../css/Register.scss';
 
 class RegisterationForm extends Component {
+  constructor(){
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      pin: "",
+      email: "",
+      username: "",
+      password: "",
+      confirmpassword: ""
+    };
+  }
+  
+  inputChanged = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
+    const errors = false;
+
     return (
       <React.Fragment>
         <section>
@@ -15,37 +36,37 @@ class RegisterationForm extends Component {
           <form>
             <div className="form-fields">
               <span><FontAwesome.FaUser /></span>
-              <input type="text" name="firstName" placeholder="First Name" />
+              <input type="text" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.inputChanged} autoComplete="firstName"/>
             </div>
             <div className="form-fields">
               <span><FontAwesome.FaUser /></span>
-              <input type="text" name="lastName" placeholder="Last Name"/>
+              <input type="text" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.inputChanged} autoComplete="lastName"/>
             </div>
             <div className="form-fields">
               <span><FontAwesome.FaMapMarker /></span>
-              <input type="text" name="pin" placeholder="Zip Code"/>
+              <input type="tel" name="pin" placeholder="Zip Code" value={this.state.pin} onChange={this.inputChanged} autoComplete="pin"/>
             </div>
             <div className="form-fields">
               <span><FontAwesome.FaEnvelope /></span>
-              <input type="text" name="email" placeholder="Email"/>
+              <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.inputChanged} autoComplete="email"/>
             </div>
             <div className="form-fields">
               <span><FontAwesome.FaUser /></span>
-              <input type="text" name="username" placeholder="Username"/>
+              <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.inputChanged} autoComplete="username"/>
             </div>
             <div className="form-fields">
               <span><FontAwesome.FaUnlockAlt /></span>
-              <input type="text" name="password" placeholder="Password"/>
+              <input type="text" name="password" placeholder="Password" value={this.state.password} onChange={this.inputChanged} autoComplete="password"/>
             </div>
             <div className="form-fields">
               <span><FontAwesome.FaLock /></span>
-              <input type="text" name="confirmpassword" placeholder="Confirm Password"/>
+              <input type="text" name="confirmpassword" placeholder="Confirm Password" value={this.state.confirmpassword} onChange={this.inputChanged} autoComplete="confirmpassword"/>
             </div>
             <div className="condition">
               <p>By registering you agree to<br/> our <b>Terms</b> and <b>Privacy Police</b></p>
             </div>
             <div className="register-button">
-              <button className="submit">Register</button>
+              <button className="submit" disabled={errors}>Register</button>
             </div>
           </form>
         </section>
