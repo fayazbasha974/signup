f5e13f09a6a95df20631c97e5a603d44b6fba1bc
jogi323 <jogigj1408@gmail.com>
2018-04-03 10:08:36 +0530

    modified wepack config and package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         test: /\.js| jsx$/,
         exclude: /node_modules/
       },
       { test: /\.css$/, loader: "style-loader!css-loader?importLoaders=1" },
-      { test: /\.(png||jpe?g|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-      {test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader?sourceMap=true' ]}
+      { test: /\.(png||jpe?g|woff|woff2|eot|ttf|svg)$/, loader: "url-loader?limit=100000" },
+      {test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader?sourceMap=true" ]}
     ]
   },
   mode: "production",
@@ -21,5 +24,14 @@ module.exports = {
     port: 8000,
     historyApiFallback: true
   },
-  performance: { hints: false }
+  performance: { hints: false },
+  plugins: [
+    new CleanWebpackPlugin('./dist'),    
+    new CopyWebpackPlugin([
+      {
+        from:"./index.html",
+        to: ""
+      }
+    ])
+  ]
 };
